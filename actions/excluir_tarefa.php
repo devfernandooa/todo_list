4574f209bc7d41e71f021891b77add716607e5c6
Fernando Almeida <devdernandooa@gmail.com>
2026-08-29 22:55:30 -0300

    Correcao action protecao exclusap tarefa

--- actions/excluir_tarefa.php
+++ actions/excluir_tarefa.php
@@ -6,11 +6,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Obtém o ID da tarefa a ser excluída
     $id_tarefa = $_POST['id_tarefa'];
 
-    // Verifica se o usuário tem permissão para excluir a tarefa
+    // Verifica se o usuário tem permissão para excluir a tarefa 
     $tarefa = buscarTarefaPorId($id_tarefa);
     if ($tarefa && $tarefa['id_usuario'] == $_SESSION['id_usuario']) {
         // Exclui a tarefa
-        if (excluirTarefa($id_tarefa)) {
+        if (excluirTarefa($id_tarefa, $_SESSION['id_usuario'])) {
             header('Location: ../views/lista_tarefas.php?sucesso=Tarefa excluída com sucesso!');
             exit();
         } else {
