2d753134770e9062add6b2bca77ce3004bce744b
Fernando Almeida <devfernandooa@gmail.com>
2026-09-25 15:31:18 -0300

    fix(auth): regenera sessão após o login

--- includes/autenticacao.php
+++ includes/autenticacao.php
@@ -15,6 +15,8 @@ function fazerLogin($email, $senha)
     //var_dump($usuario);
     // Verifica se o usuário existe e se a senha está correta
     if ($usuario && password_verify($senha, $usuario['senha'])) {
+
+        session_regenerate_id(true);
         $_SESSION['id_usuario'] = $usuario['id_usuario'];
         $_SESSION['nome'] = $usuario['nome']; // Opcional: armazenar o nome do usuário
         $_SESSION['email'] = $usuario['email'];
